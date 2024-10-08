c571c196c4d2a63382ee3df0122f5f19309bb7fa
Hod <hod@water-io.com>
2024-10-08 12:54:44 +0300

    init 2

--- app/tests/test_my_module.py
+++ app/tests/test_my_module.py
@@ -1,6 +1,7 @@
 import pytest
 from app.services.user_service import UserService
 
+
 def test_my_function():
     user_service = UserService()
     assert user_service.get_user_data() == {"username": "SampleUser", "age": 30}
